9cb9ea8bd8213dc646e01877ee7517b8aa98589c
puntual <abesabmun@alu.edu.gva.es>
2024-09-20 12:02:50 +0200

    Ejercicio24.java

---
+++ src/Ejemplo24.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Ejemplo24 {
+    public static void main(String[] args) {
+        double X = 0;
+        Scanner sc = new Scanner(System.in);
+        int Diez =0 ;
+        double notatotal=0;
+        double assignatuires=0;
+        do{
+
+            System.out.print("Dime una nota ");
+            X = sc.nextDouble();
+            if (X>-1){
+                notatotal+=X;
+                assignatuires++;
+                if(X==10){
+                    Diez++;
+                }
+            }
+        }while(X>-1);
+            sc.close();
+            System.out.println("La nota mitjana es "+notatotal/assignatuires+"Han hagut "+Diez+" deus");
+    }
+}
+
