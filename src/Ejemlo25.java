17b849ff184adfda3f90f56d03c0db6355d62955
puntual <abesabmun@alu.edu.gva.es>
2024-09-20 12:06:44 +0200

    Ejercicio25.java

---
+++ src/Ejemlo25.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejemlo25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int factorial=1;
+        int X;
+        System.out.println("Dime un numero");
+        X = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= X; i++) {
+            factorial*=i;
+
+        }
+        System.out.println(factorial);
+    }
+}
