d2d10caa904bedcec3e7b804813e0fd53eaad5a0
puntual <abesabmun@alu.edu.gva.es>
2024-09-20 12:09:29 +0200

    Ejercicio27.java

---
+++ src/Ejemplo27.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ejemplo27 {
+    public static void main(String[] args) {
+        ArrayList<Integer> lista = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int X;
+        System.out.println("Posa'm un numero");
+        X = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= X; i++) {
+            lista.add(i);
+            System.out.println(lista);
+        }
+    }
+}
