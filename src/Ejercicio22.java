b3f31bf83a600f1f301e0129d7011d6462d68968
puntual <abesabmun@alu.edu.gva.es>
2024-09-24 12:22:36 +0200

    Ejercicio28.java

--- src/Ejercicio22.java
+++ src/Ejercicio22.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Ejercicio22 {
     public static void main(String[] args) {
-        System.out.println("Ejercicio 22");
+        Scanner sc = new Scanner(System.in);
+        int positivos=0;
+        for(int i=0; i<=10;i++){
+            int X;
+            System.out.println("Dime un numero");
+            X=sc.nextInt();
+            if(X>0){
+                positivos++;
+            }
+        }
+        sc.close();
+        System.out.println(positivos);
     }
 }
