b06f89b27c5b7439a0c965d99125a0bff47b5748
puntual <abesabmun@alu.edu.gva.es>
2024-09-24 13:40:06 +0200

    Ejercicio28.java

--- src/Ejemplo26.java
+++ src/Ejemplo26.java
@@ -4,12 +4,9 @@ public class Ejemplo26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int X;
-        int y;
         System.out.println("Posa un numero per a que te digue la seua taula de multiplicar ");
         X = sc.nextInt();
-        System.out.println("Hasta quan vols que el multiplique");
-        y = sc.nextInt();
-        for(int i=1; i<=y;++i){
+        for(int i=1; i<=10;++i){
             int operacio=X*i;
             System.out.println(X+"x"+i+"="+operacio);
         }
