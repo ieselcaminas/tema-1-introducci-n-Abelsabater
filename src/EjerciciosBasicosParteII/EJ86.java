410c315732fed493f8b24bd28fa52fe3c6050206
puntual <abesabmun@alu.edu.gva.es>
2024-09-26 13:41:12 +0200

    Ejercicio28.java

---
+++ src/EjerciciosBasicosParteII/EJ86.java
@@ -0,0 +1,21 @@
+package EjerciciosBasicosParteII;
+
+import java.util.Scanner;
+
+public class EJ86 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.print("Ingrese el numero: ");
+        num = sc.nextInt();
+        do{
+            if(num%2==0){
+                num = num/2;
+                System.out.println(num);
+            }else{
+                num = num*3+1;
+                System.out.println(num);
+            }
+        }while(num!=1);
+    }
+}
