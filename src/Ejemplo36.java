7636855baaf312c7234dcbc075f424e5e6e8df9a
puntual <abesabmun@alu.edu.gva.es>
2024-09-26 12:48:31 +0200

    Ejercicio28.java

---
+++ src/Ejemplo36.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejemplo36 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int decimal;
+        System.out.println("Posa un numero decimal");
+        decimal=sc.nextInt();
+        String binari=""+""+""+""+""+""+""+"";
+        if(decimal%2!=1){
+            binari="0"+"0"+"0"+"0"+"0"+"0"+"0"+1;
+        }
+        if(decimal%2==0){
+            binari="0"+"0"+"0"+"0"+"0"+"0"+1+"0";
+        }
+        if(decimal%4==0){
+            binari="0"+"0"+"0"+"0"+"0"+1+"0"+"0";
+        }
+    }
+}
