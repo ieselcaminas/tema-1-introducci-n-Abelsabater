9f08e73d0d8f30c22f2b540ecfe1d9c8cdcab0e1
puntual <abesabmun@alu.edu.gva.es>
2024-09-27 13:02:40 +0200

    Ejercicio28.java

--- src/RefuerzoII/Ref1_21.java
+++ src/RefuerzoII/Ref1_21.java
@@ -7,13 +7,16 @@ import java.util.Scanner;
 public class Ref1_21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int salario;
         int ingresos;
         int gastos;
+        System.out.print("Ingrese el salario normal");
+        salario = sc.nextInt();
         System.out.println("Dime els ingresos que has conseguit este mes");
         ingresos = sc.nextInt();
         System.out.println("Dime els gastos de este mes");
         gastos = sc.nextInt();
-        if(ingresos < gastos) {
+        if(salario+ingresos < gastos) {
             System.out.println("No arribes");
         }else{
             System.out.println("Arribes");
