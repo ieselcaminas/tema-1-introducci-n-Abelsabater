671a72ad5ee5be0db8a777904cf00ff2a82afc6e
Abelsabater <abesabmun@alu.edu.gva.es>
2024-10-10 11:52:00 +0200

    Tema1

--- src/main/java/Ejemplos1erospasos/Ejemplo37.java
+++ src/main/java/Ejemplos1erospasos/Ejemplo37.java
@@ -8,13 +8,13 @@ public class Ejemplo37 {
         Scanner sc = new Scanner(System.in);
         int decimal=0;
         int suma;
-        char bit;
+        int bit;
         int pot;
         String bi;
         System.out.println("Posa un numero binari");
         bi=sc.nextLine();
         for (int i = 0; i <bi.length(); i++) {
-            bit= (char) (bi.charAt(i)-48);
+            bit= (bi.charAt(i)-48);
             pot= (int) Math.pow(2,(bi.length()-i-1));
             suma=bit*pot;
             decimal+=suma;
