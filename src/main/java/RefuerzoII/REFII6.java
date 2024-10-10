af52686813256e8cb22d4a00672c605269268704
Abelsabater <abesabmun@alu.edu.gva.es>
2024-10-10 12:21:32 +0200

    Tema1

--- src/main/java/RefuerzoII/REFII6.java
+++ src/main/java/RefuerzoII/REFII6.java
@@ -5,5 +5,20 @@ import java.util.Scanner;
 public class REFII6 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        String num;
+        int unidad;
+        System.out.println("Dime un número");
+        num= sc.nextLine();
+        for (int i = 0; i < num.length(); i++) {
+            unidad=num.charAt(i)-48;
+            if(unidad%2!=0){
+                System.out.println("No es hyperpar");
+                break;
+            }
+            if(unidad%2==0 && i==num.length()-1){
+                System.out.println("Es hyperpar");
+                break;
+            }
+        }
     }
 }
